2a171d69c15c6b764933eb9bef0954635f522c40
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-08 16:06:32 -0300

    update header height

--- src/app/components/sections/Header.tsx
+++ src/app/components/sections/Header.tsx
@@ -3,8 +3,8 @@ import logo from '/src/app/assets/logo-nome.png';
 
 export default function Header() {
   return (
-    <div className="header flex flex-col items-center w-full bg-zinc-100 ">
-      <a href="/">
+    <div className="header flex flex-col h-20 items-center w-full bg-zinc-100">
+      <a className="relative -top-10 " href="/">
         <Image src={logo} alt="Logotipo da Ultralog" height={150} />
       </a>
     </div>
