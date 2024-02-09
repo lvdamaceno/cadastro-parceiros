498a33e7d01f73b3f6e85ca6b7413f383de4fe52
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-09 16:47:35 -0300

    update Header

--- src/app/components/sections/Header.tsx
+++ src/app/components/sections/Header.tsx
@@ -3,11 +3,11 @@ import logo from '/src/app/assets/logo.png';
 
 export default function Header() {
   return (
-    <div className="header flex flex-col items-center w-full bg-zinc-100">
+    <div className="header flex flex-col items-center w-full bg-zinc-100 border-t-4 border-orange-600">
       <div>
-        <a className="flex items-center" href="/">
+        <a className="flex items-center" href="http://ultra.log.br">
           <p className="text-3xl font-semibold">ultra.log.br</p>
-          <Image src={logo} alt="Logotipo da Ultralog" height={80} />
+          <Image src={logo} alt="Logotipo da Ultralog" height={90} />
         </a>
       </div>
     </div>
